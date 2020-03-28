cfacea7e06aab369f586106365a20a6f4ee2d0ec
Scott Fitsimones <scott@airgara.ge>
2020-03-27 18:44:12 -0700

    update

--- covid_graph.py
+++ covid_graph.py
@@ -6,7 +6,7 @@ import plotly.io as pio
 start_date = datetime.datetime(year=2020, month=3, day=8)
 data = pd.read_csv("us-counties.csv")
 graph = px.line(data, x="date", y="cases", color="county", title="Cases by county")
-graph.update_layout(xaxis_range=['2020-03-08','2020-03-26'])
+graph.update_layout(xaxis_range=['2020-03-08','2020-03-25'])
 graph.show()
 pio.write_html(graph, file="index.html", auto_open=False)
 
