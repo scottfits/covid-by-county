2a33f82d2df7133001e9406ff3b0b3bb23ac4ed0
Scott Fitsimones <scott@airgara.ge>
2020-03-29 20:37:46 -0700

    latest data

--- covid_graph.py
+++ covid_graph.py
@@ -3,10 +3,12 @@ import plotly.express as px
 import datetime
 import plotly.io as pio
 
+DATA_URL = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+
 start_date = datetime.datetime(year=2020, month=3, day=8)
-data = pd.read_csv("us-counties.csv")
+data = pd.read_csv(DATA_URL)
 graph = px.line(data, x="date", y="cases", color="county", title="Cases by county")
-graph.update_layout(xaxis_range=['2020-03-08','2020-03-25'])
+graph.update_layout(xaxis_range=['2020-03-08','2020-03-29'])
 graph.show()
 pio.write_html(graph, file="index.html", auto_open=False)
 
